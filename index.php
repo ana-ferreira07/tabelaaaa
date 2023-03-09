ffe416cd848e574cd030adcf46a9466929759526
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-03-09 15:44:26 -0300

    tabela

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    $menu = 4;
+    switch($menu){
+        case 1;
+        case 2;
+             echo "Administracao";
+        break;
+        case 3;
+             echo "Biomedicina";
+        break;
+        case 4;
+             echo "Design Grafico";
+        break;
+        case 5;
+             echo "Física";
+        break;
+        case 6;
+             echo "Jornalismo";
+        break;
+        case 7;
+        case 8;
+        case 9;
+             echo "Medicina";
+        break;
+        case 10;
+             echo "Relacoes Públicas";
+        break;
+        default:
+             echo "Código Invalido";
+        break;
+
+    }
